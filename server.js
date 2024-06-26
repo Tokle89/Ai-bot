a00f1bf0c8f0f6dd4f66b15da36ab1784c74cc1c
Tokle89 <fredriktokle@gmail.com>
2024-06-26 19:04:24 +0200

    creates server file that sets up the node server

---
+++ server.js
@@ -0,0 +1,37 @@
+import express from "express";
+import cors from "cors";
+import { OpenAI } from "openai";
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+const apiKey = process.env.VITE_AI_KEY;
+const openai = new OpenAI({ apiKey: apiKey });
+
+app.post("/chatbot", async (req, res) => {
+  const { question } = req.body;
+
+  const response = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+
+    messages: [
+      {
+        role: "system",
+        content: "You are a helpful assistant.",
+      },
+      {
+        role: "user",
+        content: question,
+      },
+    ],
+    max_tokens: 500,
+  });
+  res.send(response.data.choices[0].message.content);
+});
